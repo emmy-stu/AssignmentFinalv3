8190978c73f5fdd52ab37780daaeab415c1a3293
emmy-stu <chillyemre@hotmail.com>
2024-10-21 15:29:09 +1100

    Imported package line

--- app/src/main/java/com/example/assignmentfinalv3/di/NetworkModule.kt
+++ app/src/main/java/com/example/assignmentfinalv3/di/NetworkModule.kt
@@ -1,2 +1,4 @@
+package com.example.assignmentfinalv3.di
+
 //In this file, we are meant to have the Network Module linking with the one in the Android Manifest file
-//However due to the complication of my lack of knowledge i couldn't get this to work
+//However due to the complications of my lack of knowledge i couldn't get this to work
